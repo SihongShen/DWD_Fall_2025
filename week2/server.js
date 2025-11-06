f4c1476125768778126a7fc63db0ebf6c4a44cf8
SihongShen <ss18264@nyu.edu>
2025-11-05 19:46:56 -0500

    week 2 in class exercise updated

--- week2/server.js
+++ week2/server.js
@@ -9,6 +9,14 @@ let app = express();
 // telling http server to serve static files from 'public' folder
 app.use(express.static('public'));
 
+// tell our express server to process incoming request bodies as JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let myObject = {
+    "secret": "This is my secret",
+}
+
 // create a get request handler at path/search
 app.get('/search', mySearchHandler);
 
@@ -18,6 +26,33 @@ function mySearchHandler(req, res){
 
     console.log('Got search request: ' + question);
     res.send("ok! What's the question?" + question);
+}
+
+let secrets = [];
+
+// build a post request handler
+app.post('/shareSecret', myShareSecretRequestHandler);
+
+function myShareSecretRequestHandler(req, res){
+    console.log('Got a secret share request!');
+
+    // get access to incoming secret
+    let secret = req.body.secret;
+    // keep track of it in the secret array
+    secrets.push(secret);
+
+    res.send("Thanks for sharing your secret!");
+}
+
+app.get('/secrets', myGetSecretsRequesthandler);
+
+function myGetSecretsRequesthandler(req, res){
+    let secretsHtml = "<h3>Secrets shared so far:</h3><ul>";
+    for(let i = 0; i < secrets.length; i++){
+        secretsHtml += "<li>" + secrets[i] + "</li>";
+    }
+    secretsHtml += "</ul>";
+    res.send(secretsHtml);
 }
 
 // listen on port 8080
